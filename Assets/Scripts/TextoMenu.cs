61736bf5a4e5858c07cf1aab32429de2a9159cc6
Bryann Alfaro <alf19372@uvg.edu.gt>
2020-06-01 16:59:37 -0600

    ready message

--- Assets/Scripts/TextoMenu.cs
+++ Assets/Scripts/TextoMenu.cs
@@ -7,35 +7,50 @@ using UnityEngine.UI;
 public class TextoMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float time = 2f; //Seconds to read the text
-    private GameObject readyText;
-    private Text texti;
+    public float time = 2.0f; //Seconds to read the text
+    private float time1 = 1.0f;
+    public GameObject readyText;
+    public GameObject flappy;
+    
 
-    public GameObject startImage;
-
+    
+    
+    
 
     void Start()
     {
-        Time.timeScale = 0.0f;
-        readyText = GameObject.FindWithTag("Text");
-        texti = readyText.GetComponent<Text>();
+        StartCoroutine("StartDelay");
+
         
-        
+    }
+
+    IEnumerator StartDelay()
+    {
+        Time.timeScale = 0;
+        float pauseTime = Time.realtimeSinceStartup + 3f;
+        while (Time.realtimeSinceStartup < pauseTime)
+        {
+            yield return 0;
+        }
+        readyText.gameObject.SetActive(false);
+        flappy.gameObject.SetActive(true);
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-            Time.timeScale = 1.0f;
-                   
-            Invoke("ChangeText", time);
-       
+
+      
+
     }
 
     void ChangeText()
     {
-        texti.text = "Go!";
-        startImage.SetActive(false);
-        Destroy(gameObject, time);
+       
+        
+
     }
+
+    
 }
